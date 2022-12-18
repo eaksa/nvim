bc7907c06ba33b344ff345bc59f694ff5a113b6a
Elang Adhyaksa <aksa@eaksa.co>
2022-12-18 16:18:32 +0700

    Initial commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+require "builtin.impatient"
+
+require "user.options"
+require "user.keymaps"
+
+require "builtin.packer"
+
+require "user.autocommands"
+require "user.colorscheme"
+
+require "builtin.core"
+require "builtin.lsp"
+require "builtin.dap"
---
+++ lua/builtin/core/init.lua
@@ -0,0 +1,17 @@
+require "builtin.core.cmp"
+require "builtin.core.telescope"
+require "builtin.core.treesitter"
+require "builtin.core.nvim-tree"
+require "builtin.core.toggleterm"
+
+require "builtin.core.autocomplete.autopairs"
+require "builtin.core.autocomplete.comment"
+
+require "builtin.core.ui.gitsigns"
+require "builtin.core.ui.bufferline"
+require "builtin.core.ui.lualine"
+require "builtin.core.ui.illuminate"
+require "builtin.core.ui.indentline"
+
+require "builtin.core.greeter.alpha"
+require "builtin.core.greeter.project"
